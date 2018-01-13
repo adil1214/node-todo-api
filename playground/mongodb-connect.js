a202945625aa045e09b197b195835b0b3ff36151
adil1214 <adil1350@gmail.com>
2018-01-13 16:43:03 +0000

    object ID's

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,12 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
 // const fs = require('fs');
+const {MongoClient, ObjectID} = require('mongodb');
+
+let obj = new ObjectID();
+console.log(obj);
+
+// let user = {name: 'Andrew', age: 25};
+// let {name} = user;      // object destructuring
 
 let url = 'mongodb://localhost:27017/TodoApp';      //  <== the database is TodoApp
 
@@ -10,7 +17,7 @@ MongoClient.connect(url, (err, db) => {
     console.log('connected to mongodb server. ');
 
 
-    // // inserting a todo into todo list
+    // // inserting a todo into 'todos' collection ======================================
     // db.collection('Todos').insertOne({
     //     text: 'Something to do',
     //     completed: false
@@ -23,18 +30,26 @@ MongoClient.connect(url, (err, db) => {
     // });
 
 
-    db.collection('Users').insertOne({
-        name: 'Mathiew',
-        age: 53,
-        address: '59 melboure street, ca USA'
-    }, (err, res) => {
-        if (err) {
-            return console.log('Unable to insert todo. error:' , err);
-        }
-        // fs.writeFileSync('log.json', JSON.stringify(res, undefined, 2));
-        // console.log(res);
-        console.log(JSON.stringify(res.ops, undefined, 2));
-    });
+    // // inserting a user into 'Users' collection =======================================
+    // db.collection('Users').insertOne({
+    //     name: 'Mathiew',
+    //     age: 53,
+    //     address: '59 melboure street, ca USA'
+    // }, (err, res) => {
+    //     if (err) {
+    //         return console.log('Unable to insert todo. error:' , err);
+    //     }
+    //     // fs.writeFileSync('log.json', JSON.stringify(res, undefined, 2));
+    //     // console.log(res);
+    //     // console.log(JSON.stringify(res.ops, undefined, 2));
+
+    //     console.log(res.ops[0]._id.getTimestamp());
+    //     // // 2018-01-13T16:31:57.000Z
+    // });
+
+
+
+
 
 
     db.close();
